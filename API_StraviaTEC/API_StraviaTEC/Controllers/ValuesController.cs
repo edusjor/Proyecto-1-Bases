9973672fff8bd2fe3973f03d768f481377b68b62
Rodolfo Bonilla <bonilla.rodolfo34@gmail.com>
2020-11-11 22:50:53 -0600

    Conexión exitosa con la base de datos postgresql, c# y el API

--- API_StraviaTEC/API_StraviaTEC/Controllers/ValuesController.cs
+++ API_StraviaTEC/API_StraviaTEC/Controllers/ValuesController.cs
@@ -4,15 +4,47 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Npgsql;
+using System.Diagnostics;
 
 namespace API_StraviaTEC.Controllers
 {
     public class ValuesController : ApiController
     {
+        NpgsqlConnection conexion = new NpgsqlConnection();
+        string cadenaConexion = "Server = 127.0.0.1; Port = 5432; Database = TallerA; User Id = postgres; Password = 123456";
+        string prueba;
         // GET api/values
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2212" };
+            conexion.ConnectionString = cadenaConexion;
+            bool flag = false;
+            var sql = "SELECT * FROM project";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conexion);
+            
+            try
+            {
+                conexion.Open();
+            }
+            catch
+            {
+                Debug.WriteLine("Usuario o contraseña incorrectos");
+                conexion.Close();
+            }
+            if (conexion.State.ToString() == "Open")
+            {
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Console.Write("{0}\t{1} \n", dr[0], dr[1]);
+                    prueba = dr[0].ToString();
+                    flag = true;
+                }
+                Debug.WriteLine("Conexion realizada con exito");
+                conexion.Close();
+                //flag = true;
+            }    
+            return prueba;
         }
 
         // GET api/values/5
