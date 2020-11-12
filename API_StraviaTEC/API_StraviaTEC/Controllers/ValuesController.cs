2fc18ed0ced85a1aa4dfed4118d78f5f97f3304b
Rodolfo Bonilla <bonilla.rodolfo34@gmail.com>
2020-11-11 21:15:31 -0600

    .

--- API_StraviaTEC/API_StraviaTEC/Controllers/ValuesController.cs
+++ API_StraviaTEC/API_StraviaTEC/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace API_StraviaTEC.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value222" };
         }
 
         // GET api/values/5
